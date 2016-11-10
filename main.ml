c92fda6f6864b5a9cff4ec516965c55e5f631a23
Vincent Lanore <vincent.lanore@unv-lyon1.fr>
2016-11-10 17:43:47 +0100

    Mostly functional pretty printer for trees.

--- main.ml
+++ main.ml
@@ -9,17 +9,33 @@ and tree =
 let pretty_print tree =
   let indent n string =
     let f x = ' ' in
-    String.concat "" ((String.init n f)::[string])
+    String.concat "" ((String.init (4*n) f)::[string])
+  in
+  let prindent n string = print_string (indent n string) in
+  let prinbase = function
+    | A -> print_string "A"
+    | T -> print_string "T"
+    | G -> print_string "G"
+    | C-> print_string "C"
   in
   let rec aux tree level =
     match tree with
     | Leaf (base) ->
-      print_string (indent level "BASE\n")
+      print_string "BASE: " ;
+      prinbase base;
+      print_string "\n"
     | Node ((f1,b1),(f2,b2)) ->
-      print_string (indent level "NODE\n") ; aux b1 (level+1) ; aux b2 (level+1)
+      print_string "NODE\n" ;
+      prindent level (string_of_float f1) ; print_string "->" ;
+      aux b1 (level+1) ;
+      prindent level (string_of_float f2) ; print_string "->" ;
+      aux b2 (level+1)
   in
   aux tree 0
 ;;
 
-let mytree = Node ( (1.0, Leaf (T) ), (2.0, Leaf (G) )  );;
+let mytree = Node (
+    (3.14, Node ((1.23, Leaf T ), (2.319, Leaf G)) ),
+    (2.4, Leaf G )
+  );;
 pretty_print mytree;;
