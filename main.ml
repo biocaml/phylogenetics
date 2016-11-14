fb3a7729d99ddbae1477194f63aafef06f08c8de
Vincent Lanore <vincent.lanore@unv-lyon1.fr>
2016-11-14 16:06:47 +0100

    Better errors + broken example on purpose + minor corrections

--- main.ml
+++ main.ml
@@ -50,32 +50,31 @@ let element_of_string s =
 ;;
 
 let tree_of_string str =
-
   let rec fulltree = function
-    | [] -> None, [] (* empty token list *)
+    | [] -> Error "Empty token list."
     | token :: rest ->
       match token with
       | Float f -> node f rest
-      | Int i -> Some (Leaf i), rest
+      | Int i -> Ok (Leaf i, rest)
 
   and node f1 = function
     | (Float f2)::rest -> left f1 f2 rest
-    | _ -> None, [] (* error expected second float *)
+    | _ -> Error "Expected second float"
 
   and left f1 f2 list =
     match fulltree list with
-    | Some tree1, rest -> right f1 f2 tree1 rest
-    | _ -> None, [] (* left tree returned unexpected result *)
+    | Ok (tree1, rest) -> right f1 f2 tree1 rest
+    | Error m -> Error (Printf.sprintf "Left returned unexpected result: <%s>" m)
 
   and right f1 f2 tree1 list =
     match fulltree list with
-    | Some tree2, rest -> Some (Node ((f1, tree1), (f2, tree2))), rest
-    | _ -> None, [] (* right tree returned unexpected result *)
+    | Ok (tree2, rest) -> Ok (Node ((f1, tree1), (f2, tree2)), rest)
+    | Error m -> Error (Printf.sprintf "Right returned unexpected result: <%s>" m)
 
   in
   match fulltree (List.map element_of_string (split_on_char ';' str)) with
-  | Some tree, _ -> tree
-  | _ -> Leaf 0
+  | Ok (t, _) -> Ok t
+  | Error m -> Error m
 ;;
 
 
@@ -89,7 +88,7 @@ let string_of_tree tree =
   let rec aux tree level =
     match tree with
     | Leaf (index) ->
-      Printf.sprintf "Base: %s\n" (string_of_int index)
+      Printf.sprintf "Index: %s\n" (string_of_int index)
     | Node ((f1,b1),(f2,b2)) ->
       Printf.sprintf "Node\n%s=(%F)=> %s%s=(%F)=> %s" (indent level) f1 (aux b1 (level+1)) (indent level) f2 (aux b2 (level+1))
   in
@@ -109,11 +108,12 @@ let jcModel a b = if a=b then -3./.4. else 1./.4. ;; (* not 100% sure about the 
 (* ========= *)
 let printline () = print_string "==========================\n";;
 printline ();;
-let mytree = tree_of_string "1.0;2.0;1;2.1;3.2;2;3" in
-print_string (string_of_tree mytree);;
+let print_treeresult = function Error m -> Printf.printf "Error: %s\n" m | Ok t -> print_string (string_of_tree t);;
+let mytree = tree_of_string "1.0;2.0;1;2.1;3.2;3" in
+print_treeresult mytree;;
 printline ();;
 let mytree = tree_of_string "3.;4.;1.;2.;11;5.;6.;12;13;7.;8.;16;9.;10.;14;15" in
-print_string (string_of_tree mytree);;
+print_treeresult mytree;;
 printline ();;
 Printf.printf "%F %F\n" (jcModel A A) (jcModel A G);;
 printline ();;
