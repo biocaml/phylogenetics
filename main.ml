23bf5d232a15ddd77ec68a9e807f8bdde1a85bbd
Vincent Lanore <vincent.lanore@unv-lyon1.fr>
2016-11-14 15:15:49 +0100

    Some code (non-functional so far) to parse text trees

--- main.ml
+++ main.ml
@@ -1,13 +1,6 @@
-(* Type for evolutionary trees with nothing at the internal
-   nodes, single bases at the leaves and length on edges (floats)*)
-type base = A | T | G | C
-and branch = float * tree
-and tree =
-  | Node of branch * branch
-  | Leaf of base
-;;
+(* base type *)
+type base = A | T | G | C;;
 
-(* Pretty printing functions for our tree type *)
 let string_of_base = function
   | A -> "A"
   | T -> "T"
@@ -17,6 +10,67 @@ let string_of_base = function
 
 let print_base base = print_string (string_of_base base);;
 
+
+(* Type for evolutionary trees: binary trees
+   whose edges are labelled with lengths (floats)
+   and whose leaves are labelled with sequence indexes (ints)*)
+type index = int
+and branch = float * tree
+and tree =
+  | Node of branch * branch
+  | Leaf of index
+;;
+
+(* Generation of tree from dfs enumeration of nodes
+   (some preliminary functions are required)*)
+let pop_char s =
+  match String.length s with
+  | 0 -> None, ""
+  | l -> Some s.[0], String.sub s (1) (l-1)
+;;
+
+let split_on_char sep s =
+  let rec aux sep s buf acc =
+    match pop_char s with
+    | None, _ -> List.rev (buf::acc)
+    | Some c, rest ->
+      if c = sep then
+        aux sep rest "" (buf::acc)
+      else
+        aux sep rest (String.concat "" [buf; String.make 1 c]) acc
+  in aux sep s "" []
+;;
+
+type element = Int of int | Float of float;;
+let element_of_string s =
+  if String.contains s '.' then
+    Float (float_of_string s)
+  else
+    Int (int_of_string s)
+;;
+
+let tree_of_string str =
+
+  let rec init = function
+    | [] -> None, [] (* empty token list *)
+    | token :: rest ->
+      match token with
+      | Float f -> node f rest
+      | Int i -> Some (Leaf i), rest
+
+  and node f1 = function
+    | (Float f2)::rest -> (
+        match init rest with
+        | Some tree, rest2 ->
+        | _ -> None, []
+      )
+    | l -> None, l (* error expected second float *)
+
+  in init (Int 1)
+;;
+
+
+(* Pretty printing functions for our tree type *)
 let indent n =
   let f x = ' ' in
   String.init (4*n) f
@@ -25,8 +79,8 @@ let indent n =
 let string_of_tree tree =
   let rec aux tree level =
     match tree with
-    | Leaf (base) ->
-      Printf.sprintf "Base: %s\n" (string_of_base base)
+    | Leaf (index) ->
+      Printf.sprintf "Base: %s\n" (string_of_int index)
     | Node ((f1,b1),(f2,b2)) ->
       Printf.sprintf "Node\n%s=(%F)=> %s%s=(%F)=> %s" (indent level) f1 (aux b1 (level+1)) (indent level) f2 (aux b2 (level+1))
   in
@@ -42,27 +96,7 @@ let jcModel a b = if a=b then -3./.4. else 1./.4. ;; (* not 100% sure about the 
 
 let probaChange m b1 b2 t = exp (-.(m b1 b2)*.t) ;;
 
-(* Some code to test all of the above *)
-let mytree = Node (
-    (3.14, Node (
-        (1.23, Leaf T ),
-        (2.3, Node (
-            (2.1, Leaf G),
-            (5.2, Leaf A)
-          )
-        )
-      )
-    ),
-    (2.3, Node (
-        (5.2, Leaf G),
-        (5.3, Leaf G)
-      )
-    )
-  );;
-
 let printline () = print_string "==========================\n";;
-printline ();;
-print_string (string_of_tree mytree);;
 printline ();;
 Printf.printf "%F %F\n" (jcModel A A) (jcModel A G);;
 printline ();;
