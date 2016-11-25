00c6eec64de686e996d6f19f2a0ccf732d23c53f
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2016-11-25 14:13:27 +0100

    Started writing documentation comments

--- lib/DNA.ml
+++ lib/DNA.ml
@@ -1,18 +1,25 @@
+(** A simple module for DNA bases (not sequences)*)
+
 open Printf
 
 type dna = A | T | G | C
 
 let string_of_dna = function A -> "A" | T -> "T" | G -> "G" | C -> "C"
 
+(** Creates a DNA base from a string containing only a single
+    letter (uppercase/lowercase).
+    Raises invalid_arg in case of incorrect string parameter.*)
 let dna_of_string = function
-  | "A" -> A
-  | "C" -> C
-  | "G" -> G
-  | "T" -> T
+  | "A" | "a" -> A
+  | "C" | "c" -> C
+  | "G" | "g" -> G
+  | "T" | "t" -> T
   | _ -> invalid_arg "dna_of_string"
 
 let int_of_dna = function A -> 0 | C -> 1 | G -> 2 | T -> 3
 
+(** Creates a DNA base from an int between 0 and 3
+    Raises invalid_arg in case of incorrect int parameter.*)
 let dna_of_int = function
   | 0 -> A
   | 1 -> C
