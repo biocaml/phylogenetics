6c9eefb25dcd5aba21b37735e7ee1d3247058377
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2016-12-01 13:55:52 +0100

    Alcotest test now complete ('make test' to test alcotest test). Pretty colours.

--- app/test_app.ml
+++ app/test_app.ml
@@ -3,11 +3,20 @@
 
 (* let mat = Mat.random 4 4;; *)
 
-open Biocaml_phylogeny ;;
-open Alcotest ;;
+open Biocaml_phylogeny
+open Alcotest
 
-let seq_test = (check string) "correct base from sequence" "C" (Sequence.test ()) ;;
+let seq_test () =
+  (check string) "get base from sequence" "C" (Sequence.test ())
 
+let seq_tests = [
+  "get", `Quick, seq_test
+]
+
+let () =
+  Alcotest.run "All tests" [
+    "sequence", seq_tests
+  ]
 
 (* TopoTree.test () ;; *)
 (* Models.test () ;; *)
