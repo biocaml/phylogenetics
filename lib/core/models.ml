c9a2105f568519a15944d72e04120d39898ccd37
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2016-12-09 11:03:28 +0100

    Models now have their mli file.

--- lib/core/models.ml
+++ lib/core/models.ml
@@ -3,6 +3,6 @@ open Sigs
 module JC69 = struct
   type t = unit
   type base = Nucleotide.t
-  let transition () a b = if a=b then -1.0 else 1./.3. (*not sure why this convention*)
+  let transition () a b = if a=b then -1.0 else 1./.3.
   let stat_dis () a = 0.25
 end
---
+++ lib/core/models.mli
@@ -0,0 +1,3 @@
+open Sigs
+
+module JC69: EVOL_MODEL with type base = Nucleotide.t and type t = unit
