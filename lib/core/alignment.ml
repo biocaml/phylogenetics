0f5ed756872a88e83594f3e5314b0f66d97b8fa9
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-02 14:19:44 +0100

    All constructors done

--- lib/core/alignment.ml
+++ lib/core/alignment.ml
@@ -6,7 +6,7 @@ open Core_kernel.Std
 module Make_hashtbl (S:SEQUENCE) = struct
   type base = S.base
   type sequence = S.t
-  type t = (string, S.t) Hashtbl.t
+  type t = (int, S.t) Hashtbl.t
   module Sequence = S
 
   let get_base tab ~seq ~pos = S.get (Hashtbl.find_exn tab seq) pos
@@ -15,6 +15,21 @@ module Make_hashtbl (S:SEQUENCE) = struct
     let align = Int.Table.create ~size:(List.length l) () in
     List.iter ~f:(fun (i,s) -> Hashtbl.add_exn ~key:i ~data:s align) l ;
     align
+
+  let of_string_list l =
+    let align = Int.Table.create ~size:(List.length l) () in
+    List.iteri ~f:(fun i s -> Hashtbl.add_exn ~key:i ~data:(S.of_string s) align) l ;
+    align
+
+  let of_fasta filename =
+    let align = Int.Table.create ~size:10 () in (* placeholder size *)
+    Fasta.with_file filename ~f:(fun _ stream ->
+        CFStream.Stream.iter ~f:(fun item ->
+            let key = Scanf.sscanf item.Biocaml_ez.Fasta.description "T%d" (fun x->x) in
+            let data = S.of_string item.Biocaml_ez.Fasta.sequence in
+            Hashtbl.add_exn ~key:key ~data:data align) stream
+      ) ;
+    align
 end
 
 module DNA_align = Make_hashtbl (Seq.DNA)
@@ -22,7 +37,9 @@ let myalign = DNA_align.of_assoc_list [
     (0, DNA_align.Sequence.of_string "ATTC");
     (1, DNA_align.Sequence.of_string "TGCA")
   ]
-let test () = DNA_align.get_base ~seq:1 ~pos:1 myalign
+let myalign2 = DNA_align.of_string_list ["ATTC"; "TGCA"]
+let myalign3 = DNA_align.of_fasta "test_data/tiny1.fasta"
+let test () = DNA_align.get_base ~seq:1 ~pos:0 myalign3
 
 
 module Make (S:SEQUENCE) = struct
