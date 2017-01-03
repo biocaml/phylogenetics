2151f9a057dfadde5da676eb477062fa4ae697aa
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-03 16:04:22 +0100

    Sequences are now arrays

--- lib/core/seq.ml
+++ lib/core/seq.ml
@@ -3,6 +3,38 @@ open Core_kernel.Std
 
 module Make (B:BASE) = struct
   type base = B.t
+  type t = {length:int ; array:base array}
+
+  let get seq i = Array.get seq.array i
+
+  let length seq = seq.length
+
+  let of_string str = {
+    length = String.length str ;
+    array = Array.init (String.length str) ~f:(
+      fun i -> B.of_char (str.[i])
+    )
+  }
+
+  let of_list l = {
+    length = List.length l ;
+    array = Array.init (List.length l) ~f:(
+      fun i -> List.nth_exn l i
+    )
+  }
+
+  let to_string seq =
+    String.init (length seq) ~f:(
+      fun i -> (B.to_string (get seq i)).[0]
+    )
+
+  let pp fmt seq =
+    to_string seq
+    |> Format.fprintf fmt "%s"
+end
+
+module Make_list (B:BASE) = struct
+  type base = B.t
   type t = base list
 
   let get seq i = match List.nth seq i with Some b->b | None->failwith "Base not found"
--- lib/core/seq.mli
+++ lib/core/seq.mli
@@ -2,4 +2,6 @@ open Sigs
 
 module Make (B:BASE): SEQUENCE with type base = B.t
 
+module Make_list (B:BASE): SEQUENCE with type base = B.t
+
 module DNA: module type of Make(Nucleotide)
