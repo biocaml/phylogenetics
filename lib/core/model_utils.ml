75eacc21f7a3f61e27eb693d5b9f1c5f11c10206
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-09 16:56:10 +0100

    Modified eMt function to allow specialization for model parameter

--- lib/core/model_utils.ml
+++ lib/core/model_utils.ml
@@ -24,12 +24,17 @@ struct
   let diag_p_inv e = init_mat Base.alphabet_size
     @@ fun x y -> E.diag_p_inv e x y
 
-  let diag e t = init_mat Base.alphabet_size
-    @@ fun x y -> if x=y then E.diag e x |> ( *. ) t |> Pervasives.exp else 0.0
+  let diag e =
+    let diag = E.diag e in
+    fun t ->
+      init_mat Base.alphabet_size
+      @@ fun x y -> if x=y then diag x |> ( *. ) t |> Pervasives.exp else 0.0
 
   let eMt_series e t = exp (scal_mat_mul (rate_matrix e) t)
 
-  let eMt e t = mult (mult (diag_p e) (diag e t)) (diag_p_inv e)
+  let eMt e =
+    let d, d_p, d_pi = diag e, diag_p e, diag_p_inv e in
+    fun t -> mult (mult d_p (d t)) d_pi
 
   let known_vector b = init_vec Base.alphabet_size
     @@ fun x->if x=Base.to_int b + 1 then 1. else 0.
