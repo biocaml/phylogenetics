601cdd0cb423e549ce7d321cae755ed91de8abbe
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-11 10:54:26 +0100

    Organized topotree mli

--- lib/core/topoTree.mli
+++ lib/core/topoTree.mli
@@ -1,12 +1,15 @@
 (** Module for phylogenetic trees. *)
 
+(** {6 Types} *)
+
 type index = string
 and branch = float * t
 and t =
   | Node of branch * branch
   | Leaf of index
 
-val pp: Format.formatter -> t -> unit
+
+(** {6 Creation/Conversion} *)
 
 val of_preorder: string -> t
 
@@ -20,9 +23,16 @@ val to_newick: t -> string
 
 val to_newick_file: t -> string -> unit
 
+
+(** {6 Parameters} *)
+
 val nb_branches: t -> int
 
 val get_branch_lengths: t -> float list
 
 val set_branch_lengths: t -> float list -> t
 
+
+(** {6 Pretty printers} *)
+
+val pp: Format.formatter -> t -> unit
