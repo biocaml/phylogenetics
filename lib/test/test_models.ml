771d3fd143a5dc87a7a68febd78ea1959ede899f
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-12 15:57:45 +0100

    Minor cleaning in test_models.ml

--- lib/test/test_models.ml
+++ lib/test/test_models.ml
@@ -1,38 +1,33 @@
 open Biocaml_phylogeny_core
 open Alcotest
-
-module K80_Utils = Models.K80
-module K80_gen_Utils = Models.K80_generated
-module JC69_Utils = Models.JC69
-module JC69_gen_Utils = Models.JC69_generated
 open LATools
+open Models
 
 let compare_matrix = check @@ testable LATools.pp_mat (LATools.compare 0.0001)
-
 
 let test_JC69_exponential () =
   compare_matrix
     "identical exponential of transition matrix"
-    (JC69_Utils.eMt_mat () 0.1)
-    (JC69_Utils.eMt_series () 0.1)
+    (JC69.eMt_mat () 0.1)
+    (JC69.eMt_series () 0.1)
 
 let test_K80_exponential () =
   compare_matrix
     "identical exponential of transition matrix"
-    (K80_Utils.eMt_mat 2.0 0.1)
-    (K80_Utils.eMt_series 2.0 0.1)
+    (K80.eMt_mat 2.0 0.1)
+    (K80.eMt_series 2.0 0.1)
 
 let test_JC69_generated () =
   compare_matrix
     "identical exponential of transition matrix"
-    (JC69_Utils.eMt_mat () 0.1)
-    (JC69_gen_Utils.eMt_mat () 0.1)
+    (JC69.eMt_mat () 0.1)
+    (JC69_generated.eMt_mat () 0.1)
 
 let test_K80_generated () =
   compare_matrix
     "identical exponential of transition matrix"
-    (K80_Utils.eMt_mat 2.0 0.1)
-    (K80_gen_Utils.eMt_mat 2.0 0.1)
+    (K80.eMt_mat 2.0 0.1)
+    (K80_generated.eMt_mat 2.0 0.1)
 
 let tests = [
   "JC69 exponential", `Quick, test_JC69_exponential;
