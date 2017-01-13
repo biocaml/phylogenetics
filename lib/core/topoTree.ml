c62a041d975bd02be868dd36901b9835227b6167
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-13 17:26:29 +0100

    Zipper is now a record

--- lib/core/topoTree.ml
+++ lib/core/topoTree.ml
@@ -14,7 +14,7 @@ and t =
   | Leaf of index
 
 type zipper =
-  | InNode of branch * branch * branch
+  | InNode of {b0:branch; b1:branch; b2:branch}
 
 
 (* ================== *)
@@ -24,24 +24,23 @@ type zipper =
 let zipper_of_tree = function
   | Node ( (l1,Node((l2,t1),(l3,t2))), (l4,t3)) |
     Node ((l4,t3), (l1,Node((l2,t1),(l3,t2))) ) ->
-    InNode ((l1 +. l4, t3), (l2,t1), (l3,t2) )
+    InNode {b0=l1+.l4, t3; b1=l2, t1; b2=l3, t2}
   | _ -> failwith "Zipper cannot be positioned at a leaf."
 
 let tree_of_zipper = function
-  | InNode ((l1, t1), (l2, t2), (l3, t3)) ->
+  | InNode {b0=l1,t1; b1=l2,t2; b2=l3,t3} ->
     Node ( (l1/.2., t1), (l1/.2., Node ((l2, t2), (l3, t3))))
 
 let branch z i = match i, z with
-  | 0, InNode (x,_,_) |
-    1, InNode (_,x,_) |
-    2, InNode (_,_,x) -> x
+  | 0, InNode {b0=x;_} |
+    1, InNode {b1=x;_} |
+    2, InNode {b2=x;_} -> x
   | i, _ -> failwith (sprintf "Invalid branch index %d. Index must be between 0 and 2." i)
 
 let move z i = match i, z with
-  | 0, InNode ( (l, Node (x,y)) ,a,b) |
-    1, InNode (a, (l, Node (x,y)) ,b) |
-    2, InNode (a,b, (l, Node (x,y)) ) ->
-    InNode (x,y, (l, Node(a,b)) )
+  | 0, InNode {b0=l,Node (x,y); b1=a; b2=b} |
+    1, InNode {b1=l,Node (x,y); b0=a; b2=b} |
+    2, InNode {b2=l,Node (x,y); b0=a; b1=b} -> InNode {b0=x; b1=y; b2=l,Node(a,b)}
   | i, _ when i>2 -> failwith (sprintf "Invalid branch index %d. Index must be between 0 and 2." i)
   | _ -> failwith "Cannot go in this direction because it is a leaf."
 
