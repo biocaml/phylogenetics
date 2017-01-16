80e7da7b2f691e6c93d85b1d7c3cd5abd02d456c
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-16 16:01:25 +0100

    branch and tree_of_zipper updated for new type

--- lib/core/zipper.ml
+++ lib/core/zipper.ml
@@ -18,25 +18,12 @@ type direction_oriented = Up | Left | Right
 type oriented_zipper = direction * t
 
 
-(* ======================= *)
-(*  CREATION / CONVERSION  *)
-(* ======================= *)
-let zipper_of_tree = function
-  | Node ( (l1,Node((l2,t1),(l3,t2))), (l4,t3)) |
-    Node ((l4,t3), (l1,Node((l2,t1),(l3,t2))) ) ->
-    InNode {b0=l1+.l4, t3; b1=l2, t1; b2=l3, t2}
-  | _ -> failwith "Zipper cannot be positioned at a leaf."
+(* ============ *)
+(*  DIRECTIONS  *)
+(* ============ *)
+let string_of_dir = function B0 -> "B0" | B1 -> "B1" | B2 -> "B2"
 
-let tree_of_zipper = function
-  | InNode {b0=l1,t1; b1=l2,t2; b2=l3,t3} ->
-    Node ( (l1/.2., t1), (l1/.2., Node ((l2, t2), (l3, t3))))
-
-let branch z i = match i, z with
-  | B0, InNode {b0=x;_} |
-    B1, InNode {b1=x;_} |
-    B2, InNode {b2=x;_} -> x
-
-let orient z d = d, z
+let dir_of_string = function "B0" -> B0 | "B1" -> B1 | "B2" -> B2 | _ -> failwith "Unexpected direction name."
 
 
 (* ========== *)
@@ -46,9 +33,9 @@ let slide z d l = match d, z with
   | B0, Leaf (i,(l2, t)) when l<l2 -> MidBranch {b0=(l2-.l, t) ; b1=(l, TopoTree.Leaf i)}
   | B0, MidBranch {b0=l1,t1; b1=l2,t2} when l<l1 -> MidBranch {b0=l1-.l,t1; b1=l2+.l,t2}
   | B1, MidBranch {b0=l1,t1; b1=l2,t2} when l<l2 -> MidBranch {b0=l1+.l,t1; b1=l2-.l,t2}
-  | B0, InNode {b0=lf,tf; b1=bb1; b2=bb2} |
-    B1, InNode {b1=lf,tf; b0=bb1; b2=bb2} |
-    B2, InNode {b2=lf,tf; b0=bb1; b1=bb2} when l<lf ->
+  | (B0, InNode {b0=lf,tf; b1=bb1; b2=bb2} |
+     B1, InNode {b1=lf,tf; b0=bb1; b2=bb2} |
+     B2, InNode {b2=lf,tf; b0=bb1; b1=bb2}) when l<lf ->
     MidBranch {b0=lf-.l,tf; b1=l,Node (bb1,bb2) }
   | _ -> failwith "Cannot slide: length too long or incorrect direction"
 
@@ -68,12 +55,32 @@ let dmove (dr,z) d =
   in B2, move z move_to
 
 
-(* ============ *)
-(*  DIRECTIONS  *)
-(* ============ *)
-let string_of_dir = function B0 -> "B0" | B1 -> "B1" | B2 -> "B2"
+(* ======================= *)
+(*  CREATION / CONVERSION  *)
+(* ======================= *)
+let zipper_of_tree = function
+  | Node ( (l1,Node((l2,t1),(l3,t2))), (l4,t3)) |
+    Node ((l4,t3), (l1,Node((l2,t1),(l3,t2))) ) ->
+    InNode {b0=l1+.l4, t3; b1=l2, t1; b2=l3, t2}
+  | _ -> failwith "Zipper cannot be positioned at a leaf."
 
-let dir_of_string = function "B0" -> B0 | "B1" -> B1 | "B2" -> B2 | _ -> failwith "Unexpected direction name."
+let rec tree_of_zipper = function
+  | (InNode {b0=l,_; _} |
+     Leaf (_,(l,_))) as z -> slide z B0 (l/.2.) |> tree_of_zipper
+  | MidBranch {b0;b1} -> Node (b0,b1)
+
+let branch z d = match d, z with
+  | _, Leaf (_,x) ->
+    if d=B0 then x
+    else failwith (sprintf "Leaf does not have a %s branch." (string_of_dir d))
+  | B0, MidBranch {b0=x;_} |
+    B1, MidBranch {b1=x;_} -> x
+  | B2, MidBranch _ -> failwith "Midbranch does not have direction B2."
+  | B0, InNode {b0=x;_} |
+    B1, InNode {b1=x;_} |
+    B2, InNode {b2=x;_} -> x
+
+let orient z d = d, z
 
 
 (* ================= *)
