9bc4f6f0656177d8a104b5e47e206699b0451f3c
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-20 11:27:33 +0100

    Comments + minor cleaning in latools.mli

--- lib/core/lATools.mli
+++ lib/core/lATools.mli
@@ -10,17 +10,16 @@ type mat
 type vec
 
 
-
 (** {6 Matrix and vector creation} *)
 
 (** Initialises a square matrix from a int->int->float function. *)
 val init_mat: int -> (int -> int -> float) -> mat
 
+(** Initializes a square diagonal matrix from the vector of its diagonal elements. *)
 val init_diag: vec -> mat
 
 (** Initialises a vector from a int->float function. *)
 val init_vec: int -> (int -> float) -> vec
-
 
 
 (** {6 Matrix and vector operations} *)
@@ -30,8 +29,10 @@ val init_vec: int -> (int -> float) -> vec
     multiplied by scalar alpha.*)
 val mult: mat -> ?alpha:float -> mat -> mat
 
+(** Matrix-vector product. *)
 val mat_vec_mul: mat -> vec -> vec
 
+(** Vector addition. *)
 val vec_vec_add: vec -> vec -> vec
 
 (** Element-wise product of two vectors. *)
@@ -54,10 +55,13 @@ val exp: mat -> mat
 (** Multiplication of a matrix by a scalar. *)
 val scal_mat_mul: mat -> float -> mat
 
+(** Scalar-vector product (in-place). *)
 val scal_vec_mul: vec -> float -> vec
 
+(** Scalar-vector product (in a copy). *)
 val scal_vec_mul_cpy: vec -> float -> vec
 
+(** Scalar-vector addition. *)
 val scal_vec_add: vec -> float -> vec
 
 (** Element-wise logarithm of matrix *)
@@ -69,20 +73,31 @@ val log_vec: vec -> vec
 (** Element-wise exponential of matrix*)
 val unlog_vec: vec -> vec
 
+(** Minimum element in a vector. *)
 val min_vec: vec -> float
 
+(** Maximum element in a vector. *)
 val max_vec: vec -> float
 
+(** Compares two matrices and tolerates a certain relative difference.
+    Let f be the float parameter, it returns true iff the elements of the second matrix
+    are between 1-f and 1+f times the corresponding elements of the first *)
 val compare: float -> mat -> mat -> bool
 
+(** Access a specific element of a vector. *)
 val get_vec: vec -> int -> float
 
+(** Access a specific element of a matrix. *)
 val get_mat: mat -> int -> int -> float
 
+(** Diagonalizes a matrix M so that M = PxDxP^T; returns P,v,P^T where
+    v is the diagonal vector of D.*)
 val diagonalize: mat -> mat * vec * mat
 
+(** Computes the inverse of a matrix. *)
 val inverse: mat -> mat
 
+(** Computes the static distribution (ie, eigenvector for eigenvalue 0) of a given matrix. *)
 val stat_dist: mat -> vec
 
 
