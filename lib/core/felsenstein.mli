838416a1f219efe0a808d01deb5c4e3357ae91a4
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-20 11:06:44 +0100

    Comments + minor cleaning in felsenstein.mli

--- lib/core/felsenstein.mli
+++ lib/core/felsenstein.mli
@@ -1,14 +1,19 @@
-open Sigs
+(** Functions that implement Felsenstein's "pruning" algorithm to compute
+    likelihood of phylogenetic trees with known sequences at leaves.*)
 
-module Make: functor (E:EVOL_MODEL) -> sig
+module Make: functor (E:Sigs.EVOL_MODEL) -> sig
 
+  (** Single-site. felsenstein without underflow prevention. *)
   val felsenstein_single : ?shift:(float -> float -> LATools.vec -> LATools.vec * float) ->
     E.t -> site:int -> TopoTree.t -> E.Align.t -> float
 
+  (** Single-site felsenstein with underflow prevention (configure threshold through threshold parameter). *)
   val felsenstein_single_shift : ?threshold:float -> E.t -> site:int -> TopoTree.t -> E.Align.t -> float
 
+  (** Multisite felsenstein without underflow prevention. *)
   val felsenstein_noshift : E.t -> TopoTree.t -> E.Align.t -> float
 
+  (** Multisite felsenstein with underflow prevention (use this by default). *)
   val felsenstein : E.t -> TopoTree.t -> E.Align.t -> float
 
 end
