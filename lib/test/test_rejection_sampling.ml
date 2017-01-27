44f9d1ea018d397483e6372614737f0fdd9a45f7
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-01-27 14:51:46 +0100

    Minor cleaning and improvements in test_rejection_sampling

--- lib/test/test_rejection_sampling.ml
+++ lib/test/test_rejection_sampling.ml
@@ -11,12 +11,18 @@ let check_distrib = (* In practice, it just compares two lists of floats. *)
   (check @@ list (testable (pp Alcotest.float) (float_compare 0.05)))
     "Distributions with identical characteristics"
 
+
+(** {6 Test input parameters} *)
+
 module RS_DNA = Make(Models.K80)
 let myalign = RS_DNA.Align.of_string_list ["A";"A";"A";"T"]
 let mybasetree = TopoTree.of_preorder "0.1;0.1;0.1;0.1;0;1;2.5;0.1;2;3"
 let mysampler = Stat_tools.sample_branch_lengths ~branchs:(fun i -> i=5) ~sampler:(Stat_tools.sample_float_uniform 5.0) mybasetree
 
-let test amount =
+
+(** {6 Test functions} *)
+
+let sample amount =
   let prior_trees = RS_DNA.generate_trees ~sampler:mysampler amount in
   let post_trees = RS_DNA.reject 2.0 myalign prior_trees in
   let mean_prior, mean_post, acceptance =
@@ -27,6 +33,9 @@ let test amount =
   in [mean_prior; mean_post; acceptance]
 
 let test_rejection () =
-  check_distrib [2.5;2.8;0.012] (test 500000)
+  check_distrib [2.5;2.8;0.012] (sample 500000)
 
-let tests = ["Basic test", `Slow, test_rejection]
+
+(** {6 Test list} *)
+
+let tests = ["Specific branch length on small tree with 500k points.", `Slow, test_rejection]
