6533cab13f43ea174ee8c2e1e18fbd2d1f520a9e
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-02-01 17:12:57 +0100

    Minor cleaning

--- lib/core/zipper.ml
+++ lib/core/zipper.ml
@@ -258,14 +258,14 @@ let equal z1 z2 = match z1, z2 with
     Phylogenetic_tree.(i=i2 && (get_meta t).id = (get_meta t2).id && b=b2)
   | ZipBranch {b0=b00,t00; b1=b01,t01; _}, ZipNode {b0=b10,t10; b1=b11,t11; _} ->
     Phylogenetic_tree.(
-      (get_meta t00).id = (get_meta t10).id && b00=b10 &&
-      (get_meta t01).id = (get_meta t11).id && b01=b11
+      (get_meta t00).id = (get_meta t10).id && b00 = b10 &&
+      (get_meta t01).id = (get_meta t11).id && b01 = b11
     )
   | ZipNode {b0=b00,t00; b1=b01,t01; b2=b02,t02; _}, ZipNode {b0=b10,t10; b1=b11,t11; b2=b12,t12; _} ->
     Phylogenetic_tree.(
-      (get_meta t00).id = (get_meta t10).id && b00=b10 &&
-      (get_meta t01).id = (get_meta t11).id && b01=b11 &&
-      (get_meta t02).id = (get_meta t12).id && b02=b12
+      (get_meta t00).id = (get_meta t10).id && b00 = b10 &&
+      (get_meta t01).id = (get_meta t11).id && b01 = b11 &&
+      (get_meta t02).id = (get_meta t12).id && b02 = b12
     )
   | _ -> false (* FIXME enumerate cases *)
 
