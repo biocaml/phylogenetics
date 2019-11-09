076b5bba0da45e896cf0407940724403bdbd295c
Philippe Veber <philippe.veber@gmail.com>
2019-11-09 09:20:05 +0100

    new Amino_acid module (from convdet)

---
+++ lib/amino_acid.ml
@@ -0,0 +1,28 @@
+open Base
+
+include Alphabet.Make(struct let card = 20 end)
+
+let chars_of_aa_string = "ACDEFGHIKLMNPQRSTVWY"
+
+let%test "chars_of_aa_string" =
+  String.length chars_of_aa_string = card
+
+let to_char i = chars_of_aa_string.[i]
+
+let code_A = Char.to_int 'A'
+
+let aa_of_chars =
+  let t = Array.create ~len:26 None in
+  for i = 0 to String.length chars_of_aa_string - 1 do
+    t.(Char.to_int chars_of_aa_string.[i] - code_A) <- Some i
+  done ;
+  t
+
+let of_char = function
+  | 'A'..'Z' as c -> aa_of_chars.(Char.to_int c - code_A)
+  | _ -> None
+
+let%test "of_char" =
+  let open Poly in
+  of_char (to_char 19) = Some 19
+  && of_char 'Z' = None
---
+++ lib/amino_acid.mli
@@ -0,0 +1,4 @@
+include Alphabet.S_int
+
+val to_char : t -> char
+val of_char : char -> t option
