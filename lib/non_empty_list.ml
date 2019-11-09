86b1179fd868dfe71d6f5664931100393ab70ab5
Philippe Veber <philippe.veber@gmail.com>
2019-11-09 20:42:03 +0100

    Non_empty_list: added iter and init

--- lib/non_empty_list.ml
+++ lib/non_empty_list.ml
@@ -2,6 +2,10 @@ open Core_kernel
 
 type 'a t = Cons of 'a * 'a list
 
+let init n ~f =
+  if n < 1 then invalid_arg "Non_empty_list should be at least of length 1" ;
+  Cons (f 0, List.init (n - 1) ~f:(fun i -> f (i + 1)))
+
 let cons h t = Cons (h, t)
 
 let fold (Cons (h, t)) ~init ~f =
@@ -9,6 +13,9 @@ let fold (Cons (h, t)) ~init ~f =
 
 let map (Cons (h, t)) ~f =
   cons (f h) (List.map t ~f)
+
+let iter (Cons (h, t)) ~f =
+  f h ; List.iter t ~f
 
 let to_list (Cons (h, t)) =
   h :: t
--- lib/non_empty_list.mli
+++ lib/non_empty_list.mli
@@ -1,5 +1,7 @@
 type 'a t = Cons of 'a * 'a list
 val cons : 'a -> 'a list -> 'a t
+val init : int -> f:(int -> 'a) -> 'a t
 val map : 'a t -> f:('a -> 'b) -> 'b t
+val iter : 'a t -> f:('a -> unit) -> unit
 val fold : 'a t -> init:'c -> f:('c -> 'a -> 'c) -> 'c
 val to_list : 'a t -> 'a list
