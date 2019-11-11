5f1a42bddcd8b3bc87733adecbc0ce932164896a
Philippe Veber <philippe.veber@gmail.com>
2019-11-11 15:34:20 +0100

    Bench: stat_dist
    For a matrix of size 4:
    ┌───────────────────────┬──────────┬─────────┬────────────┐
    │ Name                  │ Time/Run │ mWd/Run │ Percentage │
    ├───────────────────────┼──────────┼─────────┼────────────┤
    │ Lacaml_stat_dist_test │   4.18us │ 137.00w │     79.22% │
    │ Owl_stat_dist_test    │   5.27us │ 452.01w │    100.00% │
    └───────────────────────┴──────────┴─────────┴────────────┘
    
    For a matrix of size 100:
    ┌───────────────────────┬────────────┬─────────┬──────────┬──────────┬────────────┐
    │ Name                  │   Time/Run │ mWd/Run │ mjWd/Run │ Prom/Run │ Percentage │
    ├───────────────────────┼────────────┼─────────┼──────────┼──────────┼────────────┤
    │ Lacaml_stat_dist_test │ 1_076.96us │ 137.15w │    0.91w │    0.91w │    100.00% │
    │ Owl_stat_dist_test    │   763.36us │ 452.22w │    1.54w │    1.54w │     70.88% │
    └───────────────────────┴────────────┴─────────┴──────────┴──────────┴────────────┘

--- app/bench_app.ml
+++ app/bench_app.ml
@@ -1,5 +1,6 @@
 open Core
 open Core_bench
+open Phylogenetics
 
 let a = Lacaml.D.Mat.hilbert 100
 
@@ -17,11 +18,44 @@ let owl_mat_mat_mul_test =
     ~name:"Owl_mat_mat_mul"
     (fun () -> Owl.Mat.dot a' a')
 
+let b, b' =
+  let n = 100 in
+  Lacaml.D.Mat.init_rows n n (fun i j -> if i = j then -1. else 1. /. (float n -. 1.)),
+  Linear_algebra.Mat.init n ~f:(fun i j -> if i = j then -1. else 1. /. (float n -. 1.))
+
+let lacaml_stat_dist_test =
+  Bench.Test.create
+    ~name:"Lacaml_stat_dist_test"
+    (fun () ->
+       let open Lacaml.D in
+       let stat_dist m =
+         (* copy matrix to avoid erasing original *)
+         let tmp = lacpy m in
+         (* get eigenvector for eigenvalue 0 *)
+         match syevr ~vectors:true ~range:(`V(-0.001,0.001)) tmp with
+         | (_,_,c,_) ->
+           let vec = Mat.col c 1 in
+           (* normalize so the sum of elements equals 1 *)
+           Linear_algebra_tools.Lacaml.(scal_vec_mul (1./.(Vec.sum vec)) vec)
+       in
+       stat_dist b
+    )
+
+let owl_stat_dist_test =
+  Bench.Test.create
+    ~name:"Owl_stat_dist_test"
+    (fun () -> Linear_algebra.Mat.zero_eigen_vector b')
+
 let tests = [
   lacaml_mat_mat_mul_test ;
   owl_mat_mat_mul_test ;
+  lacaml_stat_dist_test ;
+  owl_stat_dist_test ;
 ]
 
-let command = Bench.make_command tests
+let command = Command.group ~summary:"Performance benches" [
+    "mat-mul", Bench.make_command [ lacaml_mat_mat_mul_test ; owl_mat_mat_mul_test ] ;
+    "stat-dist", Bench.make_command [ lacaml_stat_dist_test ; owl_stat_dist_test ] ;
+  ]
 
 let () = Command.run command
