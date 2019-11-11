b89d8a2dade7b360eac16cbe46d9d9051e33a51b
Philippe Veber <philippe.veber@gmail.com>
2019-11-11 16:55:25 +0100

    Bench: added mat_vec_mul Lacaml vs Owl
    ┌────────────────────┬──────────┬─────────┬────────────┐
    │ Name               │ Time/Run │ mWd/Run │ Percentage │
    ├────────────────────┼──────────┼─────────┼────────────┤
    │ Lacaml_mat_vec_mul │   6.33us │  22.00w │     97.75% │
    │ Owl_mat_vec_mul    │   6.47us │ 148.00w │    100.00% │
    └────────────────────┴──────────┴─────────┴────────────┘
    
    ┌────────────────────┬──────────┬─────────┬────────────┐
    │ Name               │ Time/Run │ mWd/Run │ Percentage │
    ├────────────────────┼──────────┼─────────┼────────────┤
    │ Lacaml_mat_vec_mul │   6.52us │  22.00w │    100.00% │
    │ Owl_mat_vec_mul    │   6.46us │ 148.00w │     99.14% │
    └────────────────────┴──────────┴─────────┴────────────┘

--- app/bench_app.ml
+++ app/bench_app.ml
@@ -8,12 +8,12 @@ let a' =
   Lacaml.D.Mat.to_array a
   |> Owl.Mat.of_arrays
 
-let lacaml_mat_mat_mul_test =
+let lacaml_mat_mat_mul =
   Bench.Test.create
     ~name:"Lacaml_mat_mat_mul"
     (fun () -> Lacaml.D.gemm a a)
 
-let owl_mat_mat_mul_test =
+let owl_mat_mat_mul =
   Bench.Test.create
     ~name:"Owl_mat_mat_mul"
     (fun () -> Owl.Mat.dot a' a')
@@ -23,9 +23,9 @@ let b, b' =
   Lacaml.D.Mat.init_rows n n (fun i j -> if i = j then -1. else 1. /. (float n -. 1.)),
   Linear_algebra.Mat.init n ~f:(fun i j -> if i = j then -1. else 1. /. (float n -. 1.))
 
-let lacaml_stat_dist_test =
+let lacaml_stat_dist =
   Bench.Test.create
-    ~name:"Lacaml_stat_dist_test"
+    ~name:"Lacaml_stat_dist"
     (fun () ->
        let open Lacaml.D in
        let stat_dist m =
@@ -41,21 +41,33 @@ let lacaml_stat_dist_test =
        stat_dist b
     )
 
-let owl_stat_dist_test =
+let owl_stat_dist =
   Bench.Test.create
-    ~name:"Owl_stat_dist_test"
+    ~name:"Owl_stat_dist"
     (fun () -> Linear_algebra.Mat.zero_eigen_vector b')
 
-let lacaml_expm_test =
+let lacaml_expm =
   Bench.Test.create ~name:"Lacaml_expm" (fun () -> Linear_algebra_tools.Lacaml.Mat.expm a)
 
-let owl_expm_test =
+let owl_expm =
   Bench.Test.create ~name:"Owl_expm" (fun () -> Owl.Linalg.D.expm a')
 
+let v, v' =
+  let n = Lacaml.D.Mat.dim1 a in
+  Lacaml.D.Vec.init n float,
+  (Linear_algebra.Vec.init n ~f:float :> Owl.Mat.mat)
+    
+let lacaml_mat_vec_mul =
+  Bench.Test.create ~name:"Lacaml_mat_vec_mul" (fun () -> Linear_algebra_tools.Lacaml.mat_vec_mul a v)
+
+let owl_mat_vec_mul =
+  Bench.Test.create ~name:"Owl_mat_vec_mul" (fun () -> Owl.Mat.dot a' v')
+
 let command = Command.group ~summary:"Performance benches" [
-    "mat-mul", Bench.make_command [ lacaml_mat_mat_mul_test ; owl_mat_mat_mul_test ] ;
-    "stat-dist", Bench.make_command [ lacaml_stat_dist_test ; owl_stat_dist_test ] ;
-    "expm", Bench.make_command [lacaml_expm_test ; owl_expm_test] ;
+    "mat-mul", Bench.make_command [ lacaml_mat_mat_mul ; owl_mat_mat_mul ] ;
+    "stat-dist", Bench.make_command [ lacaml_stat_dist ; owl_stat_dist ] ;
+    "expm", Bench.make_command [lacaml_expm ; owl_expm] ;
+    "mat-vec-mul", Bench.make_command [lacaml_mat_vec_mul ; owl_mat_vec_mul] ;
   ]
 
 let () = Command.run command
