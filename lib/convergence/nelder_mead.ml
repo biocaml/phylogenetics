657adb3c2deadc24e75cff5fe77bd8904052af40
Philippe Veber <philippe.veber@gmail.com>
2019-12-08 12:15:36 +0100

    convergence: added Nelder_mead algorithm from convdet

---
+++ lib/convergence/nelder_mead.ml
@@ -0,0 +1,113 @@
+(**
+   Implements method as described in https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
+   some tests from original publication:
+
+     A simplex method for function minimization
+     J. A. Nelder and R. Mead
+*)
+open Core_kernel
+
+let centroid xs =
+  let n = Array.length xs in
+  if n = 0 then raise (Invalid_argument "Nelder_mead.centroid: empty array") ;
+  let d = Array.length xs.(0) in
+  Array.init d ~f:(fun i ->
+      Array.fold xs ~init:0. ~f:(fun acc x -> acc +. x.(i))
+      /. float n
+    )
+
+let update ~from:c alpha ~towards:x =
+  let d = Array.length c in
+  Array.init d ~f:(fun i -> c.(i) +. alpha *. (x.(i) -. c.(i)))
+
+let minimize ?(tol = 1e-8) ?(maxit = 100_000) ?(debug = false) ~f ~sample () =
+  let alpha = 1. in
+  let gamma = 2. in
+  let rho = 0.5 in
+  let sigma = 0.5 in
+  let x0 = sample () in
+  let n = Array.length x0 in
+  if n = 0 then raise (Invalid_argument "Nelder_mead.minimize: sample returns empty vectors") ;
+  let sample () =
+    let y = sample () in
+    if Array.length y <> n then raise (Invalid_argument "Nelder_mead.minimize: sample returns vectors of varying lengths") ;
+    y
+  in
+  let points = Array.init (n + 1) ~f:(fun _ -> sample ()) in
+  let obj = Array.map points ~f in
+  let rec loop i =
+    let ranks = Owl_utils.Array.argsort ~cmp:Float.compare obj in
+    if debug then (
+      printf "\n\nIteration %d: %f %s\n%!" i obj.(ranks.(0)) (Phylogenetics.Utils.show_float_array points.(ranks.(0)))
+    ) ;
+    let c =
+      Array.slice ranks 0 (-1)
+      |> Array.map ~f:(Array.get points)
+      |> centroid
+    in
+    let x_r = update ~from:c (-. alpha) ~towards:points.(ranks.(n)) in
+    let f_r = f x_r in
+    if debug then (
+      printf "Candidate: %f %s\n" f_r (Phylogenetics.Utils.show_float_array x_r)
+    ) ;
+    (
+      match f_r < obj.(ranks.(0)), f_r < obj.(ranks.(n - 1)) with
+      | false, true ->
+        if debug then printf "Reflection\n" ;
+        points.(ranks.(n)) <- x_r ;
+        obj.(ranks.(n)) <- f_r ;
+      | true, _ ->
+        if debug then printf "Expansion\n" ;
+        let x_e = update ~from:c gamma ~towards:x_r in
+        let f_e = f x_e in
+        points.(ranks.(n)) <- if f_e < f_r then x_e else x_r ;
+        obj.(ranks.(n)) <- Float.min f_r f_e ;
+      | false, false ->
+        if debug then printf "Contraction\n" ;
+        let x_c = update ~from:c rho ~towards:points.(ranks.(n)) in
+        let f_c = f x_c in
+        if f_c < f_r then (
+          points.(ranks.(n)) <- x_c ;
+          obj.(ranks.(n)) <- f_c ;
+        )
+        else (
+          if debug then printf "Shrink\n" ;
+          Array.iteri points ~f:(fun i x_i ->
+              if ranks.(i) > 0 then (
+                let x_i = update ~from:points.(ranks.(0)) sigma ~towards:x_i in
+                points.(i) <- x_i ;
+                obj.(i) <- f x_i
+              )
+            )
+        )
+    ) ;
+    let sigma = Owl_base_stats.std obj in
+    if Float.(sigma < tol) || i >= maxit then obj.(ranks.(0)), points.(ranks.(0))
+    else loop (i + 1)
+  in
+  loop 0
+
+let%test "Parabola" =
+  let f x = x.(0) ** 2. in
+  let sample () = [| Random.float 200. -. 100. |] in
+  let obj, _ = minimize ~f ~tol:1e-3 ~sample () in
+  Float.abs obj < 1e-3
+
+let%test "Rosenbrock" =
+  let f x = 100. *. (x.(1) -. x.(0) ** 2.) ** 2. +. (1. -. x.(0)) ** 2. in
+  let rfloat _ = Random.float 200. -. 100. in
+  let sample () = Array.init 2 ~f:rfloat in
+  let obj, _ = minimize ~f ~sample () in
+  Float.abs obj < 1e-3
+
+let%test "Powell quartic" =
+  let f x =
+    let open Float in
+    (x.(0) + 10. * x.(1)) ** 2. + 5. *. (x.(2) - x.(3)) ** 2.
+    + (x.(1) - 2. *. x.(2)) ** 4. + 10. * (x.(0) - x.(3)) ** 4.
+  in
+  let rfloat _ = Random.float 200. -. 100. in
+  let sample () = Array.init 4 ~f:rfloat in
+  let obj, _ = minimize ~f ~sample () in
+  Float.abs obj < 1e-3
+
