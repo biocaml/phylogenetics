biocaml/phylogenetics

commit 2f597852286e953aa9edb72b029a8fe6c0cde750

Fitch: support for cost function
added an argument to express that transitions may have different
costs. For instance, if the character is a phenotype, gain is
typically more costly than loss.

lib/fitch.ml
@@ -1,6 +1,9 @@
 open Core_kernel
 
-let rec forward ~n ~category (t : (_,'l,_) Tree.t) =
+let default_cost x y =
+  if x = y then 0. else 1.
+
+let rec forward ?(cost = default_cost) ~n ~category (t : (_,'l,_) Tree.t) =
   match t with
   | Leaf l ->
     let costs =
@@ -23,9 +26,7 @@ let rec forward ~n ~category (t : (_,'l,_) Tree.t) =
       Array.init n ~f:(fun i ->
           let costs_for_root_i =
             List1.map children_costs ~f:(fun costs ->
-                let cost j =
-                  if i = j then costs.(j) else costs.(j) +. 1.
-                in
+                let cost j = costs.(j) +. cost i j in
                 let rec loop j best_cost best_choice =
                   if j = n then (best_cost, best_choice)
                   else
@@ -59,8 +60,8 @@ let backward costs t =
   let root = Owl.Utils.Array.min_i ~cmp:Float.compare costs in
   backward_aux t root
 
-let fitch ~n ~category t =
-  let costs, routing = forward ~n ~category t in
+let fitch ?cost ~n ~category t =
+  let costs, routing = forward ?cost ~n ~category t in
   backward costs routing
 
 let%expect_test "fitch" =

lib/fitch.mli
@@ -1,4 +1,5 @@
 val forward :
+  ?cost:(int -> int -> float) ->
   n:int ->
   category:('l -> int option) ->
   ('n, 'l, 'b) Tree.t ->
@@ -10,6 +11,7 @@ val backward :
   ('n * int, 'l * int, 'b) Tree.t
 
 val fitch :
+  ?cost:(int -> int -> float) ->
   n:int ->
   category:('l -> int option) ->
   ('n, 'l, 'b) Tree.t ->

commit cfed38beff6f713e698d77e8c45dfa49b1b04e6c

Fitch: fixed bug introduced in 2f59785

lib/fitch.ml
@@ -17,7 +17,7 @@ let rec forward ?(cost = default_cost) ~n ~category (t : (_,'l,_) Tree.t) =
   | Node node ->
     let children_costs, children =
       List1.map node.branches ~f:(fun (Branch b) ->
-          let cost, child = forward ~n ~category b.tip in
+          let cost, child = forward ~cost ~n ~category b.tip in
           cost, Tree.branch b.data child
         )
       |> List1.unzip