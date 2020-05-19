dc12ac3cf9dfcca6ad93b4cc2fd642927dfb692e
Philippe Veber <philippe.veber@gmail.com>
2020-05-19 20:44:12 +0200

    Phylogenetics: added simple sequence generator

---
+++ lib/sequence_simulator.ml
@@ -0,0 +1,42 @@
+open Core_kernel
+
+module type Alphabet = sig
+  type t
+  val card : int
+  val of_int_exn : int -> t
+  val to_char : t -> char
+end
+
+module Make(A : Alphabet) = struct
+  type profile = Profile of {
+      probs : float array ;
+      dist : Gsl.Randist.discrete ;
+    }
+
+  let of_array_exn probs =
+    if Array.length probs <> A.card then failwith "Array has incorrect size" ;
+    let dist = Gsl.Randist.discrete_preproc probs in
+    Profile { probs ; dist }
+
+  let random_profile ~alpha rng =
+    let alpha = Array.create ~len:A.card alpha in
+    let theta = Array.create ~len:A.card 0. in
+    Gsl.Randist.dirichlet rng ~alpha ~theta ;
+    of_array_exn theta
+    
+  let draw_from_profile (Profile p) rng =
+    Gsl.Randist.discrete rng p.dist
+    |> A.of_int_exn
+
+  type pwm = profile array
+
+  let random_pwm ~alpha n rng =
+    Array.init n ~f:(fun _ -> random_profile ~alpha rng)
+
+  let draw_from_pwm pwm rng =
+    let n = Array.length pwm in
+    String.init n ~f:(fun i ->
+        draw_from_profile pwm.(i) rng
+        |> A.to_char
+      )
+end
---
+++ lib/sequence_simulator.mli
@@ -0,0 +1,20 @@
+module type Alphabet = sig
+  type t
+  val card : int
+  val of_int_exn : int -> t
+  val to_char : t -> char
+end
+
+module Make(A : Alphabet) : sig
+  type profile = Profile of {
+      probs : float array ;
+      dist : Gsl.Randist.discrete ;
+    }
+  val of_array_exn : float array -> profile
+  val random_profile : alpha:float -> Gsl.Rng.t -> profile
+  val draw_from_profile : profile -> Gsl.Rng.t -> A.t
+
+  type pwm = profile array
+  val random_pwm : alpha:float -> int -> Gsl.Rng.t -> pwm
+  val draw_from_pwm : pwm -> Gsl.Rng.t -> string
+end
