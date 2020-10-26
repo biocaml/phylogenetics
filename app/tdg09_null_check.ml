5bb05eb7fff7eeacb8ab36e35cd3d81303c1c6be
Philippe Veber <philippe.veber@gmail.com>
2020-10-26 11:51:49 +0100

    app/Tdg09_check_null: M1 vs M2

--- app/tdg09_null_check.ml
+++ app/tdg09_null_check.ml
@@ -1,6 +1,14 @@
 open Core_kernel
 open Phylogenetics_convergence
 
+let lrt_1_vs_2 () =
+  let open Tdg09.Implementation_check in
+  let wag = Phylogenetics.Wag.parse "wag.dat" in
+  lrt_1_vs_2_null_simulation ~mode:`dense ~nb_simulations:100 wag
+  |> Fn.flip
+    (render_pvalue_histogram ~title:"M1 vs M2 under H0")
+    "model1_vs_model2_pvalues_under_h0.pdf"
+
 let lrt_2_vs_3 () =
   let open Tdg09.Implementation_check in
   let wag = Phylogenetics.Wag.parse "wag.dat" in
@@ -9,5 +17,4 @@ let lrt_2_vs_3 () =
     (render_pvalue_histogram ~title:"M2 vs M3 under H0")
     "model2_vs_model3_pvalues_under_h0.pdf"
 
-let () =
-  lrt_2_vs_3 ()
+let () = lrt_1_vs_2 ()
