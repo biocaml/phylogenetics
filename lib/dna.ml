cc0d18253e91dadadfedee6d988857ecdab0f99c
Louis Duchemin <louis.duchemin@univ-lyon1.fr>
2021-03-29 11:46:03 +0200

    Dna.of_codons implementation

--- lib/dna.ml
+++ lib/dna.ml
@@ -14,6 +14,12 @@ let of_string_exn s =
   | None -> s
   | Some c -> invalid_argf "of_string_exn: unexpected character '%c'" c ()
 
+
+let of_codons codons = 
+  Array.map codons ~f:Codon.Universal_genetic_code.NS.to_string
+  |> String.concat_array
+  |> of_string_exn
+
 let gc_contents s =
   let n = String.count s ~f:(function
       | 'C' | 'G' -> true
--- lib/dna.mli
+++ lib/dna.mli
@@ -2,4 +2,5 @@ type t = private string
 
 val of_string_unsafe : string -> t
 val of_string_exn : string -> t
+val of_codons : Codon.Universal_genetic_code.NS.t array -> t
 val gc_contents : t -> float
