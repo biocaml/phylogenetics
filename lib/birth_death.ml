92d9c7e5ef3faf4a5dd06cc329d0897f089fa6ab
Philippe Veber <philippe.veber@gmail.com>
2021-04-15 11:27:16 +0200

    Birth_death: fixed various bugs

--- lib/birth_death.ml
+++ lib/birth_death.ml
@@ -13,18 +13,21 @@ let make ~birth_rate ~death_rate =
 
 let simulation p rng ~time =
   let open ID_monad in
-  let rec branch t =
-    let next_birth = Randist.exponential rng ~mu:p.birth_rate in
-    let next_death = Randist.exponential rng ~mu:p.death_rate in
+  let birth_mean_time = 1. /. p.birth_rate
+  and death_mean_time = 1. /. p.death_rate in
+  let rec branch remaining_time =
+    let next_birth = Randist.exponential rng ~mu:birth_mean_time in
+    let next_death = Randist.exponential rng ~mu:death_mean_time in
     let next_event = Float.min next_birth next_death in
     let* id = new_id in
-    if Float.(t +. next_event > time) then
-      return @@ Tree.branch (time -. t) (Tree.leaf id)
+    if Float.(next_event > remaining_time) then
+      return @@ Tree.branch remaining_time (Tree.leaf id)
     else if Float.(next_birth < next_death) then
-      let* left_branch = branch (t -. next_birth) in
-      let+ right_branch = branch (t -. next_birth) in
+      let remaining_time' = remaining_time -. next_event in
+      let* left_branch = branch remaining_time' in
+      let+ right_branch = branch remaining_time' in
       Tree.branch next_birth (Tree.binary_node id left_branch right_branch)
     else
       return @@ Tree.branch next_death (Tree.leaf id)
   in
-  run (branch 0.)
+  run (branch time)
