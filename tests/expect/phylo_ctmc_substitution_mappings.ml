0253e43ad8f4aa16c063a2d8bf05f48fae36db98
Philippe Veber <philippe.veber@gmail.com>
2021-06-05 13:47:34 +0200

    mappings: test that show convergence to pruning-based likelihood

--- tests/expect/phylo_ctmc_substitution_mappings.ml
+++ tests/expect/phylo_ctmc_substitution_mappings.ml
@@ -13,7 +13,7 @@ module Branch_info = struct
   let length x = x
 end
 
-module AASim = Simulator.Make(Amino_acid)(Branch_info)
+module AASim = Phylogenetics.Simulator.Make(Amino_acid)(Branch_info)
 
 let valine = Option.value_exn (Amino_acid.of_char 'V')
 
@@ -47,28 +47,74 @@ let sample_site tree root =
   let rates = rate_matrix wag_param in
   AASim.site_gillespie_first_reaction rng tree ~root ~rate_matrix:(fun _ -> rates)
 
+let iter_branches t ~f =
+  let rec traverse_node = function
+    | Tree.Leaf _ -> ()
+    | Node n ->
+      List1.iter n.branches ~f:(traverse_branch n.data)
+  and traverse_branch parent_data (Tree.Branch b) =
+    f parent_data b.data (Tree.data b.tip) ;
+    traverse_node b.tip
+  in
+  traverse_node t
+
+let sufficient_statistics ~nstates tree =
+  let counts = Array.make_matrix ~dimx:nstates ~dimy:nstates 0 in
+  let waiting_times = Array.create ~len:nstates 0. in
+  iter_branches tree ~f:(fun start_state (bl, mapping) _ ->
+      match mapping with
+      | [||] -> ()
+      | _ ->
+        Array.iteri mapping ~f:(fun k (s_j, t_j)  ->
+            let s_i, t_i =
+              if k = 0 then start_state, 0.
+              else mapping.(k -1)
+            in
+            counts.(s_i).(s_j) <- 1 + counts.(s_i).(s_j) ;
+            waiting_times.(s_i) <- waiting_times.(s_i) +. (t_j -. t_i)
+          ) ;
+        let (last_state, last_time) = Array.last mapping in
+        waiting_times.(last_state) <- waiting_times.(last_state) +. bl -. last_time
+    ) ;
+  counts, waiting_times
+
+let mapping_likelihood ~nstates ~rate_matrix tree =
+  let counts, waiting_times = sufficient_statistics ~nstates tree in
+  let lik = ref 0. in
+  for i = 0 to nstates - 1 do
+    for j = 0 to nstates - 1 do
+      let contrib =
+        if i = j then waiting_times.(i) *. Matrix.get rate_matrix i i
+        else float counts.(i).(j) *. Float.log (Matrix.get rate_matrix i j)
+      in
+      lik := !lik +. contrib
+    done ;
+  done ;
+  !lik
+
 let () =
   let tree = sample_tree () in
   let site = sample_site tree valine in
   let nstates = Amino_acid.card in
   let transition_matrix = (transition_matrix wag_param :> float -> Matrix.t) in
-  let conditional_simulation =
-    Phylo_ctmc.conditionial_likelihoods site ~nstates ~leaf_state:Amino_acid.to_int ~transition_matrix
-    |> Phylo_ctmc.conditional_simulation rng ~root_frequencies:(wag.freqs :> Vector.t)
+  let leaf_state = Amino_acid.to_int in
+  let root_frequencies =  (wag.freqs :> Vector.t) in
+  let conditional_likelihoods =
+    Phylo_ctmc.conditionial_likelihoods site ~nstates ~leaf_state ~transition_matrix
   in
+  let rate_matrix = (rate_matrix wag_param :> Matrix.t) in
   let process =
-    let p = Phylo_ctmc.uniformized_process (rate_matrix wag_param :> Matrix.t) in
+    let p = Phylo_ctmc.uniformized_process rate_matrix in
     fun _ -> p
   in
-  let mapping = Phylo_ctmc.substitution_mapping ~rng ~branch_length:Fn.id ~nstates ~process conditional_simulation in
-  let mean_number_of_internal_events =
-    let sum, n =
-      Tree.prefix_traversal mapping
-        ~init:(0., 0)
-        ~branch:(fun (sum, n) (bl, history) -> sum +. float (Array.length history) /. bl, n + 1)
-        ~node:(fun acc _ -> acc)
-        ~leaf:(fun acc _ -> acc)
-    in
-    sum /. float n
+  let mean_mapping_likelihood =
+    Array.init 1_000 ~f:(fun _ ->
+        Phylo_ctmc.conditional_simulation rng ~root_frequencies conditional_likelihoods
+        |> Phylo_ctmc.substitution_mapping ~rng ~branch_length:Fn.id ~nstates ~process
+        |> mapping_likelihood ~nstates ~rate_matrix
+      )
+    |> Gsl.Stats.mean
   in
-  printf "%f\n" mean_number_of_internal_events
+  printf "%f %f\n"
+    (Phylo_ctmc.pruning site ~nstates ~transition_matrix ~leaf_state ~root_frequencies)
+    mean_mapping_likelihood
