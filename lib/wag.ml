bcef13cb3754584cf7739f7a4c79d8a49c2d785f
Louis Duchemin <louis.duchemin@univ-lyon1.fr>
2021-10-20 15:43:26 +0200

    Generalize exchangeability matrix parsing

--- lib/wag.ml
+++ lib/wag.ml
@@ -11,7 +11,7 @@ let parse_aa_order l =
       let aa = Amino_acid.to_char aa in
       match String.lfindi chars ~f:(fun _ c -> Char.equal c aa) with
       | Some i -> i
-      | None -> failwith chars
+      | None -> failwith l
     )
 
 let parse_floats l =
@@ -19,7 +19,7 @@ let parse_floats l =
   |> List.filter ~f:(String.( <> ) "")
   |> List.map ~f:Float.of_string
   |> Array.of_list
-  
+
 let parse_freqs aa_order l =
   let values = parse_floats l in
   Amino_acid.Vector.init (fun aa -> values.(Amino_acid.Table.get aa_order aa))
@@ -32,15 +32,22 @@ let parse_rate_matrix aa_order lines =
       values.(max i j - 1).(min i j)
     )
 
-let parse fn =
+let from_lines lines =
   let lines =
-    In_channel.read_lines fn
+    List.filter lines ~f:(Fn.non String.is_empty)
     |> Array.of_list
   in
   let rate_matrix_lines = Array.sub lines ~pos:0 ~len:19 in
-  let freqs_line = lines.(20) in
-  let aa_line = lines.(23) in
+  let freqs_line = lines.(19) in
+  let aa_line = lines.(20) in
   let aa_order = parse_aa_order aa_line in
   let freqs = parse_freqs aa_order freqs_line in
   let rate_matrix = parse_rate_matrix aa_order rate_matrix_lines in
   { rate_matrix ; freqs }
+
+let from_string_exn s =
+  String.split_lines s |> from_lines
+let from_file_exn fn =
+  In_channel.read_lines fn |> from_lines
+
+let parse fn = from_file_exn fn
