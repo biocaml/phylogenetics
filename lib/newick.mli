05cee391898d9c851f237ceba970fbe5abb19cd4
Philippe Veber <philippe.veber@gmail.com>
2022-04-17 12:21:35 +0200

    Newick: resurrected Tree-based representation

--- lib/newick.mli
+++ lib/newick.mli
@@ -10,3 +10,24 @@ val from_string_exn : string -> t
 
 val to_string : t -> string
 val to_file : t -> string -> unit
+
+module Tree_repr : sig
+  type ast = t
+
+  type node_info = {
+    name : string option ;
+    tags : tag list ;
+  }
+
+  type tree = (node_info, node_info, float option) Tree.t
+  type branch = (node_info, node_info, float option) Tree.branch
+
+  type t =
+    | Tree of tree
+    | Branch of branch
+
+  val of_ast : ast -> t
+
+  val map_inner_tree : t -> f:(tree -> tree) -> t
+  val with_inner_tree : t -> f:(tree -> 'a) -> 'a
+end
