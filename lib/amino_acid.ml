a443c9a3c6f9ce75b8186588fd657787225b6c8d
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2022-06-27 16:08:34 +0200

    Implement JSON conversion for AA vector

--- lib/amino_acid.ml
+++ lib/amino_acid.ml
@@ -1,13 +1,38 @@
-open Base
-
+open Core
 include Alphabet.Make(struct let card = 20 end)
 
 let chars_of_aa_string = "ACDEFGHIKLMNPQRSTVWY"
+
 
 let%test "chars_of_aa_string" =
   String.length chars_of_aa_string = card
 
 let to_char i = chars_of_aa_string.[i]
+
+let yojson_of_vector vec : Yojson.Safe.t =
+  `Assoc (
+    Vector.to_array vec
+    |> Array.mapi ~f:(fun aa x -> to_char aa |> Char.to_string, `Float x)
+    |> Array.to_list
+  )
+
+let vector_of_yojson aa_list =
+  (* Optimization to validate that all AA are present *)
+  let aa_queue =
+    Yojson.Safe.Util.to_assoc aa_list
+    |> List.sort ~compare:(fun (x, _) (y, _) -> String.compare x y)
+    |> Queue.of_list in
+  Vector.init (fun aa ->
+      let aa_char = to_char aa in
+      let is_expected aa_str = Char.(aa_str |> of_string = aa_char) in
+      match Queue.dequeue aa_queue with
+      | Some (key, x) when is_expected key -> Yojson.Safe.Util.to_number x
+      | Some (key, _ ) ->
+        failwithf "Expected value for %c ; found %s" aa_char key ()
+      | None ->
+        failwithf "Missing AA : %c ; reached end of json record without finding it"
+          aa_char ()
+    )
 
 let code_A = Char.to_int 'A'
 
--- lib/amino_acid.mli
+++ lib/amino_acid.mli
@@ -1,5 +1,8 @@
 include Alphabet.S_int
 
+val yojson_of_vector : vector -> Yojson.Safe.t
+val vector_of_yojson : Yojson.Safe.t -> vector
+
 val to_char : t -> char
 val of_char : char -> t option
 val of_char_exn : char -> t
