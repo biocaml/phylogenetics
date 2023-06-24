0ff89d62184df3ec9225e28c91bd1f46f3f25903
Philippe Veber <philippe.veber@gmail.com>
2023-06-24 08:34:00 +0200

    fixed odoc warnings

--- lib/wag.mli
+++ lib/wag.mli
@@ -9,7 +9,7 @@ type t = {
 }
 
 val parse : string -> t
-[@@@ocaml.alert deprecated "Use from_file_exn"]
+[@@ocaml.alert deprecated "Use from_file_exn"]
 
 val from_file_exn : string -> t
 val from_string_exn : string -> t
